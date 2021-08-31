05a7d538b94eb3b836c6ad4e4fafc872fc0dee65
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-31 15:18:10 +0530

    Add

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-//PATTERNS
-var a = "";
-var i = 5;
-while (i >= 0) {
-  if (i === 0) {
-    var k = 1;
-    while (k <= 5) {
-      var m = 1;
-      while (m <= k) {
-        a = a + "*";
-        m++;
-      }
-      console.log(a);
-      var a = "";
-      k++;
-    }
-  } else {
-    var j = 1;
-    while (j <= i) {
-      a = a + "*";
-      j++;
-    }
-    console.log(a);
-    a = "";
-  }
-  i--;
-}
+//PATTERNS ********************************************************************
+// var a = "";
+// var i = 5;
+// while (i >= 0) {
+//   if (i === 0) {
+//     var k = 1;
+//     while (k <= 5) {
+//       var m = 1;
+//       while (m <= k) {
+//         a = a + "*";
+//         m++;
+//       }
+//       console.log(a);
+//       var a = "";
+//       k++;
+//     }
+//   } else {
+//     var j = 1;
+//     while (j <= i) {
+//       a = a + "*";
+//       j++;
+//     }
+//     console.log(a);
+//     a = "";
+//   }
+//   i--;
+// }
 
 // var n = 125;
 // var a = Math.cbrt(11);
