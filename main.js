4a81c2d2791aa13f91ef013b4bff7545fd26a326
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-10-16 14:14:04 +0530

    Add

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-const a = [1, 2, 3, 4, 2];
+const a = ["1", "2", "3"];
 const ans = a.sort((b, a) => {
   return a - b;
 });
-
 console.log(a);
 
+// const timestamp = ;
+const newTm = new Date(parseInt(Date.now())).toLocaleTimeString();
+console.log(newTm);
+
+const b =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quod beatae numquam asperiores doloremque dolorem officia, totam necessitatibus nobis nam?";
+console.log(b.substring(0, 20));
+
+const image = [
+  "https://funzumo.com/wp-content/uploads/2019/06/Best-Funny-Minion-Quotes.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSikt2YTSQ9lQ9m193gW7YirTtypIWmh5NcIrL4jJGwTHQMVc365k_pJJpNoed1ii98Qrs&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvtt6IOe4Gp9Q2lcLdjEKmpreGojqeq4ZYLZSoOgrkyx_v6r6YWpo6JuHT0yW5vRRiA7g&usqp=CAU",
+  "https://saltedcaramel670.files.wordpress.com/2019/04/minion-holding-banana.jpg?w=940&h=627&crop=1",
+  "https://cdn.shopify.com/s/files/1/2284/6393/products/tonies-electronics-tonie-despicable-me-minions-28186416185431.jpg?v=1628453229",
+  "https://i.ebayimg.com/images/g/RNsAAOSwGotWq6ju/s-l300.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwZf6vuPh0QAtebimDXbQwuipikvGiK3QWvgY9IvzehXJBpaAJFNZ2cWG6feKmoCw81g0&usqp=CAU",
+  "https://i.pinimg.com/originals/a9/ec/f2/a9ecf2d6cdf8b364c3edd1bb513229e6.jpg",
+];
+
+const random = image[Math.floor(Math.random() * image.length)];
+console.log(random);
 // ******************** REGEX ***********************
 // let str = "The String";
 // const regex = /string/i;
