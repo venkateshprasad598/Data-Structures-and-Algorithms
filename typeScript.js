a5b591f7b521ed51b13066f64d192a0bf413365e
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-10-25 16:13:36 +0530

    add

---
+++ typeScript.js
@@ -0,0 +1,7 @@
+console.log("Hello Typescript");
+var a = "9";
+console.log(a);
+var str = 10;
+console.log(str);
+var boo = true;
+console.log("boolean", boo);
--- typeScript.ts
+++ typeScript.ts
@@ -1,1 +1,9 @@
-console.log("Typescript")
+console.log("Hello Typescript")
+let a : string = "9"
+console.log(a)
+
+let str : number = 10
+console.log(str)
+
+let boo : boolean = true
+console.log("boolean", boo)
