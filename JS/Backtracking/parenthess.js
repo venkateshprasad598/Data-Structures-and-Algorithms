e15725721ab6a7648c495c49947a2304e1531615
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-12-07 12:12:00 +0530

    message

--- JS/Backtracking/parenthess.js
+++ JS/Backtracking/parenthess.js
@@ -20,4 +20,26 @@ function parenthesses(n) {
   console.log(arr);
 }
 parenthesses(2);
-console.log("Hello");
+
+console.log("parenthess");
+function parenthesses(n) {
+  let curString = "";
+  let open, close;
+  let arr = [];
+  function backTrack(curString, open, close, max) {
+    if (curString.length == max * 2) {
+      arr.push(curString);
+      return;
+    }
+
+    if (open < max) {
+      backTrack(curString + "(", open + 1, close, max);
+    }
+    if (close < open) {
+      backTrack(curString + ")", open, close + 1, max);
+    }
+  }
+  backTrack(curString, 0, 0, n);
+  console.log(arr);
+}
+parenthesses(2);
